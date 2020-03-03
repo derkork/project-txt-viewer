a462402b96a7b84c410e14bae5e5e1a38c264eb5
Jan Thomä <janthomae@janthomae.de>
2020-03-03 08:57:41 +0100

    chore: initial typescript setup

---
+++ src/components/EssentialLink.vue
@@ -0,0 +1,55 @@
+<template>
+  <q-item
+    clickable
+    tag="a"
+    target="_blank"
+    :href="link"
+  >
+    <q-item-section
+      v-if="icon"
+      avatar
+    >
+      <q-icon :name="icon" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label>{{ title }}</q-item-label>
+      <q-item-label caption>
+        {{ caption }}Cäptain!
+      </q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+const EssentialLinkProps = Vue.extend({
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+
+    caption: {
+      type: String,
+      default: ''
+    },
+
+    link: {
+      type: String,
+      default: '#'
+    },
+
+    icon: {
+      type: String,
+      default: ''
+    }
+  }
+});
+
+@Component
+export default class EssentialLink extends EssentialLinkProps {
+}
+</script>
